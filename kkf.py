968f6824409c79df6ce6d5d6d62f529b5a9b208b
C0A22133 <c0a221335a@edu.teu.ac.jp>
2023-12-19 17:49:31 +0900

    ガード機能の追加

--- kkf.py
+++ kkf.py
@@ -11,10 +11,12 @@ HEIGHT = 900  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
 
-class Koukaton:
+class Koukaton(pg.sprite.Sprite):
     def __init__(self):
+        super().__init__()
         self.hp = 100
         self.speed = 1.0
+        self.damage = 0
         
     def setHp(self, hp):
         self.hp = hp
@@ -25,6 +27,30 @@ class Koukaton:
         self.speed = speed
     def getSpeed(self):
         return self.speed
+    
+    def setDamage(self, damage: int):
+        self.damage = damage
+    def getDamage(self):
+        return self.damage
+    
+class Guard(pg.sprite.Sprite):
+    """
+    ガードに関するクラス
+    """
+    def __init__(self,):
+        super().__init__()
+        self.guard_hp = 5
+
+    def update(self, screen: pg.Surface,  koukaton: Koukaton):
+        if self.guard_hp <= 0:  #ガードが不可能な場合
+            koukaton.setSpeed(1.0)  #行動不可を解除
+        else:
+            if koukaton.getDamage() > 0:
+                koukaton.setDamage(0)  #ダメージの無効化
+                self.guard_hp -= 1  #ガード可能回数を減らす
+            koukaton.setSpeed(0.0)  #こうかとんを移動不可にする
+            pg.draw.circle(screen, (0, 255, 255), (500,500), 20 * self.guard_hp)  #ガード表示
+                   
 
 def main():
     pg.display.set_caption("大戦争スマッシュこうかとんファイターズ")
@@ -33,13 +59,30 @@ def main():
 
     tmr = 0
     clock = pg.time.Clock()
+    guard = Guard()
 
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: return
-
+            if event.type == pg.QUIT:
+                return
+        koukaton = Koukaton()
         screen.blit(bg_img, [0, 0])
         #メイン処理
+        ###キーボード処理###
+        key_lst = pg.key.get_pressed()
+
+        #テスト用
+        if key_lst[pg.K_e]:
+            if tmr % 50 == 0:
+                koukaton.setDamage(100)
+        #テスト用終わり
+            
+        if key_lst[pg.K_q]:
+            guard.update(screen, koukaton)
+        else:
+            guard = Guard()
+
+        print(koukaton.getDamage())
 
         pg.display.update()
         tmr += 1
