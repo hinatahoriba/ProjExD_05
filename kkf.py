a9de24d1613dab5d3b9acb558df3a69c71a394a5
C0A22133 <c0a221335a@edu.teu.ac.jp>
2023-12-19 14:45:15 +0900

    初期状態

---
+++ kkf.py
@@ -0,0 +1,39 @@
+import math
+import os
+import random
+import sys
+import time
+from typing import Any
+import pygame as pg
+
+WIDTH = 1600  # ゲームウィンドウの幅
+HEIGHT = 900  # ゲームウィンドウの高さ
+MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
+
+def main():
+    pg.display.set_caption("大戦争スマッシュこうかとんファイターズ")
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    bg_img = pg.image.load(f"{MAIN_DIR}/fig/pg_bg.jpg")
+
+    tmr = 0
+    clock = pg.time.Clock()
+
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        #メイン処理
+
+        pg.display.update()
+        tmr += 1
+        clock.tick(50)
+            
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
+
+    
