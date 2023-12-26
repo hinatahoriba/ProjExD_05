4566ae33c0b20c44c20744e549b2887b40ad7d92
fushimity@edu.teu.ac.jp <c0a22145bf@edu.teu.ac.jp>
2023-12-26 13:41:11 +0900

    追加機能：ゲーム設定実装完了

--- kkf.py
+++ kkf.py
@@ -10,6 +10,10 @@ WIDTH = 1600  # ゲームウィンドウの幅
 HEIGHT = 900  # ゲームウィンドウの高さ
 MAIN_DIR = os.path.split(os.path.abspath(__file__))[0]
 
+def draw_start_screen(screen, font, text, color):
+        text_render = font.render(text, True, color)
+        text_rect = text_render.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+        screen.blit(text_render, text_rect)
 
 class Koukaton:
     def __init__(self):
@@ -60,7 +64,7 @@ class start:
             if self.reset_timer <= 0:
                 self.reset()
             else:
-                self.reset_timer -= dt       
+                self.reset_timer -= dt
 
 
 def main():
@@ -76,8 +80,34 @@ def main():
     hyper_font = pg.font.Font(None, 50)  # 残り時間用のフォント
     hyper_color = (0, 0, 255)  # 残り時間の表示色
     fonto = pg.font.Font(None, 200)  # ゲームオーバーの文字を生成
-    txt = fonto.render("Game Over", True, (255, 0, 0))
+    txt = fonto.render("Time UP", True, (255, 0, 0))
+    start_screen_font = pg.font.Font(None, 200)
+    start_screen_color = (200, 50, 100)  # 紫色の文字
+    lnvalid_screen_color = (255, 0, 0)  # 赤色の文字)
+    
+    # スタート画面についての部分
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
 
+        screen.blit(bg_img, [0, 0])
+
+        keys = pg.key.get_pressed()
+
+        if keys[pg.K_1]: # 「１」を押すとメインコード部分が読み込み開始
+            draw_start_screen(screen, start_screen_font, "Game start", start_screen_color)
+            pg.display.flip()
+            pg.time.delay(2000)  # 表示を少し待つ
+            break
+        else: # 「１」が押されるまでの画面表示
+            draw_start_screen(screen, start_screen_font, "Are you ready?", lnvalid_screen_color)
+            pg.display.flip()
+    tmr = 0
+    clock.tick(50)
+    # ここまでがスタート画面です。
+
+    # メインコード部分
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT:
@@ -90,31 +120,37 @@ def main():
         tmr += 1
         clock.tick(50)
 
-        dt = 20 - tmr/50 # ゲームの経過時間を計算
+        dt = 10 - tmr/50 # ゲームの経過時間を計算
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 pg.quit()
                 sys.exit()
 
-        # キー入力の処理
+
+        # キー入力の処理 HPが減るかの確認用
         keys = pg.key.get_pressed() # キーボードの状態をゲットする
         if vict_condition.allow_input: # 勝利条件が満たされているか
             if keys[pg.K_SPACE]: # スペースキーが押された場合hp減少
-                koukaton.hp -= 10
+                koukaton.setHp(koukaton.getHp() - 10)   
+                print(koukaton.getHp())
         
-        if dt >= 0:
+        if dt >= 0: # 時間が0になるまでの時間を右下に表示
             hyper_text = hyper_font.render(f"Time: {int(dt)}", True, hyper_color)
             hyper_pos = (WIDTH - hyper_text.get_width() - 10, HEIGHT - hyper_text.get_height() - 10)
             screen.blit(hyper_text, hyper_pos)  # 残り時間を表示
-        elif dt <= 0:
+        elif dt <= 0: # 時間が0になった時に「Time UP」と表示
             txt_rect = txt.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-            screen.blit(txt, txt_rect)  # Game Overを画面中央に表示
+            screen.blit(txt, txt_rect)  # Time UPを画面中央に表示
             pg.display.update()
             pg.time.delay(2000)
             return
-            
-            
+        if koukaton.getHp() <= 0:
+            txt_rect = txt.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+            draw_start_screen(screen, start_screen_font, "Finish!!!", lnvalid_screen_color)  # Finissh!!!を画面中央に表示
+            pg.display.update()
+            pg.time.delay(2000)
+            return   
         
         # 勝利条件の更新
         vict_condition.update(dt)
